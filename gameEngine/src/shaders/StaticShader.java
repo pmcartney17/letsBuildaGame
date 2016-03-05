ed9628b11d6798af5d72ab79592b1a19cc209e2f
Ian Moody <imoody1@hotmail.com>
2016-03-05 14:43:20 -0700

    changed shader.txt directory

--- gameEngine/src/shaders/StaticShader.java
+++ gameEngine/src/shaders/StaticShader.java
@@ -3,8 +3,8 @@ package shaders;
 public class StaticShader extends ShaderProgram {
 
 	
-	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
-	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+	private static final String VERTEX_FILE = "bin/shaders/vertexShader.txt";
+	private static final String FRAGMENT_FILE = "bin/shaders/fragmentShader.txt";
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
 	
